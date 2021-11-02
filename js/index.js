1536bb695e8f724626df4c7f18345c545a452260
Daniil Pautkin <pautkin2003@gmail.com>
2021-11-02 12:17:43 +0200

    Add Clock

--- js/index.js
+++ js/index.js
@@ -4,3 +4,16 @@ let changeBG = () => {
     !!i ? $("body").css('background', 'darkgrey') : $("body").css('background', 'white')
     i = !i
 }
+
+window.onload = function() {
+    window.setInterval(function() {
+        var now = new Date();
+        var clock = document.getElementById("clock");
+        clock.innerHTML = now.toLocaleTimeString();
+    }, 1000);
+};
+
+let clockCSS = () => {
+    !!i ? $("#clock").css('color', 'orange') : $("#clock").css('color', 'black')
+    i = !i
+}
